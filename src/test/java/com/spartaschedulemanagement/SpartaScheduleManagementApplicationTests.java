60c02a8549812284da2f4fb20c57897880762f52
sangmu <dltkdandl@naver.com>
2025-11-05 19:49:00 +0900

    SpartScheduleManagement 프로젝트 시작 및 초기설정

---
+++ src/test/java/com/spartaschedulemanagement/SpartaScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaschedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
